df2041ecdcfed12ebabec7bed67f5c1b386b1ba5
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-04-30 07:06:05 +0900

    Convert the repository name to lowercase for using Bitbucket API

--- gtob.sh
+++ gtob.sh
@@ -60,6 +60,7 @@ EOF
 check_required_items() {
     if [ -z "$gh_repo_url" ]; then
         read -p "GitHub repository URL: " gh_repo_url
+        echo
 
         if [ -z "$gh_repo_url" ]; then
             echo -e $color_error"error: GitHub repository URL is required"$color_reset 1>&2
@@ -69,6 +70,7 @@ check_required_items() {
 
     if [ -z "$bb_username" ]; then
         read -p "Your Bitbucket username: " bb_username
+        echo
 
         if [ -z "$bb_username" ]; then
             echo -e $color_error"error: Bitbucket username is required"$color_reset 1>&2
@@ -78,6 +80,7 @@ check_required_items() {
 
     if [ -z "$bb_password" ]; then
         read -sp "Your Bitbucket password: " bb_password
+        echo
 
         if [ -z "$bb_password" ]; then
             echo -e $color_error"error: Bitbucket password is required"$color_reset 1>&2
@@ -95,6 +98,8 @@ analyze_url() {
 # git clone from GitHub
 clone() {
     tmp_repo_path="$local_repos_path/$gh_repo_name"
+    mkdir -p tmp_repo_path
+
     if [ -n "$gh_repo_url" ]; then
         git clone $gh_repo_url $tmp_repo_path --branch $branch_name
     fi
@@ -109,18 +114,18 @@ wiki_push() {
     if [ ! -e $tmp_wiki_path ]; then
         echo -e $color_error"error: wiki directory is not exist"$color_reset 1>&2
     else
-        cd $tmp_wiki_path
-        git remote set-url origin "$bb_ssh_url:$bb_username/$gh_repo_name.git/wiki"
+        cd $tmp_wiki_path && \
+        git remote set-url origin "$bb_ssh_url:$bb_username/$bb_repo_name.git/wiki"
         if [ ! -e Home.md ]; then
-            echo "# $gh_repo_name  
+            echo "# $bb_repo_name  
 ### Page List  
-https://bitbucket.org/$bb_username/$gh_repo_name/wiki/browse/" > Home.md
+https://bitbucket.org/$bb_username/$bb_repo_name/wiki/browse/" > Home.md
             git add Home.md
             git commit -m "Add Home.md"
         else
             echo "  
 ### Page List  
-https://bitbucket.org/$bb_username/$gh_repo_name/wiki/browse/" >> Home.md
+https://bitbucket.org/$bb_username/$bb_repo_name/wiki/browse/" >> Home.md
             git add Home.md
             git commit -m "Update Home.md"
         fi
@@ -149,21 +154,24 @@ submodule() {
 }
 
 # create Bitbucket repository
-create_repo() {
-    bb_response=`curl -X POST "$bb_api_url/repositories/$bb_username/$gh_repo_name" \
+create_bb_repository() {
+    bb_repo_name=`echo $gh_repo_name | tr "[:upper:]" "[:lower:]"` # Slugs must be lowercase
+
+    bb_api_response=`curl -X POST "$bb_api_url/repositories/$bb_username/$bb_repo_name" \
     --user "$bb_username:$bb_password" \
     -H "Content-type: application/json" \
-    -d "{\"scm\": \"git\", \"is_private\": \"$is_private\", \"name\": \"$gh_repo_name\", \"has_wiki\": \"$migrate_wiki\"}"`
+    -d "{\"scm\": \"git\", \"is_private\": \"$is_private\", \"name\": \"$bb_repo_name\", \"has_wiki\": \"$migrate_wiki\"}"`
 
+    echo "$bb_api_response"
     # TODO: if receive {"type": "error", "error": {"fields": {"name": ["You already have a repository with this name."]}, "message": "Repository with this Slug and Owner already exists."}} then
-    # echo -e $color_warn"warn: \"$gh_repo_name\" repository is already exists in Bitbucket"$color_reset
+    # echo -e $color_warn"warn: \"$bb_repo_name\" repository is already exists in Bitbucket"$color_reset
 }
 
 # git push to Bitbucket
 push() {
-    cd $tmp_repo_path
-    git remote set-url $remote_url_name "$bb_ssh_url:$bb_username/$gh_repo_name.git"
-    git push --set-upstream $remote_url_name $branch_name --force
+    cd $tmp_repo_path && \
+    git remote set-url $remote_url_name "$bb_ssh_url:$bb_username/$bb_repo_name.git" && \
+    git push --set-upstream $remote_url_name $branch_name --force && \
     git push $remote_url_name --tags
     cd $current_dir
 }
@@ -174,7 +182,7 @@ migrate() {
     analyze_url $gh_repo_url
     clone
     submodule
-    create_repo
+    create_bb_repository
     push
     if $migrate_wiki; then
         wiki
